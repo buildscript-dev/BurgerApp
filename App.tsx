fb2d78a458f66e202cbcf364cdbe84ac8fa32140
buildscript-dev <buildscript.dev@gmail.com>
2025-06-13 23:38:15 +0530

    Navigation Setuped

--- App.tsx
+++ App.tsx
@@ -1,13 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import HomePage from './components/(Tabs)/homePage';
+import './gesture-handler';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
